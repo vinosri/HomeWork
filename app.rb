170b1f70d16bc61b0ed92d1b3fabeb30f8dd0c09
vinosri <vinods.scm@gmail.com>
2016-04-14 23:40:04 -0600

    Hello team yours as variable commented

--- app.rb
+++ app.rb
@@ -4,7 +4,8 @@ $i= 0
 get '/' do
   	 time = Time.now
   	 $i +=1
-  	 'Hello world! The current time is ' +time.strftime('%H:%M')+ 'Hello Team! Your time is ' ' GMT.' +time.strftime('%H:%M')+ ',' $i '.'
+  	 'Hello world! The current time is ' +time.strftime('%H:%M')+ 'Hello Team! Your time is ' ' GMT.' +time.strftime('%H:%M')+ ','
+  	 # $i '.'
  end
 
 

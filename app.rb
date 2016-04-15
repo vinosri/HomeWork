d5f4e20f0e99f460f8247f12ffeae8f4d83c5a9a
vinosri <vinods.scm@gmail.com>
2016-04-14 22:20:41 -0600

    visitor added as single line

--- app.rb
+++ app.rb
@@ -5,8 +5,7 @@ visitor = 0
 get '/' do
   	 time = Time.now
   	 Visitor += 1
-  	'Hello world! The current time is ' +time.strftime('%H:%M')+ ' GMT' '.' 
-  	'You are Visitor Number ' +visitor.to_s+ '.'
+  	'Hello world! The current time is ' +time.strftime('%H:%M')+ ' GMT' ', You are Visitor Number ' +visitor.to_s+ '.'
 end
 
 

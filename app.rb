87714873a44bfb34f1ed111597745330ccd49a7b
vinosri <vinods.scm@gmail.com>
2016-04-14 23:22:59 -0600

    Hello team yours as comments in between

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require 'sinatra'
 
-$visitor = 0
+#$visitor = 0
 get '/' do
   	 time = Time.now
-  	 $visitor += 1
-  	 'Hello world! The current time is ' +time.strftime('%H:%M')+ ' GMT.'  $visitor
+  	 #$visitor +=1
+  	 'Hello world! The current time is ' +time.strftime('%H:%M')+ ' GMT.'  
+  	 #$visitor
   	 #'Hello Team! Your are visitor number ' +time.strftime('%H:%M')+  '.'
  end
 

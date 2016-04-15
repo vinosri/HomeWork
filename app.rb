4cb7b9dea575f9a7167c1c44cabd2b594ae77026
vinosri <vinods.scm@gmail.com>
2016-04-14 21:43:01 -0600

    visitor function merged with time function

--- app.rb
+++ app.rb
@@ -1,12 +1,10 @@
 require 'sinatra'
-get '/' do
-  	 time = Time.now
-  	'Hello world! The current time is ' +time.strftime('%H:%M')+ ' GMT' '.' 
-end
 
 visitor = 0
 
 get '/' do
+  	 time = Time.now
+  	'Hello world! The current time is ' +time.strftime('%H:%M')+ ' GMT' '.' 
   	 Visitor += 1
 	'You are Visitor Number ' +visitor.to_s+'.'
 end

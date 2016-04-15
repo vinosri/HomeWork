61c5fa2f9320df8294ec7691e942a522b150b877
vinosri <vinods.scm@gmail.com>
2016-04-14 23:52:05 -0600

    Hello team local time

--- app.rb
+++ app.rb
@@ -4,9 +4,9 @@ $i= 0
 get '/' do
   	 time = Time.now
   	 $i +=1
-  	 puts  $i
-  	 'Hello world! The current time is ' +time.strftime('%H:%M')+ '; Hello Team! Your time is' 'GMT' +time.strftime('%H:%M')+ ','
-  	 puts ' Great' $i
+  	 #puts  $i
+  	 'Hello world! The current time is ' +time.strftime('%H:%M')+ '; Hello Team! Your time is'  +time.strftime('%H:%M')+ ','
+  	 #puts ' Great' $i
   	 # $i '.'
  end
 

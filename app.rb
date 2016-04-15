a718d178d70263f2039bb6adb4058fc33d5ffaff
vinosri <vinods.scm@gmail.com>
2016-04-14 23:27:13 -0600

    Hello team yours as new line

--- app.rb
+++ app.rb
@@ -6,7 +6,8 @@ get '/' do
   	 #$visitor +=1
   	 'Hello world! The current time is ' +time.strftime('%H:%M')+ ' GMT.'  
   	 #$visitor
-  	 #'Hello Team! Your are visitor number ' +time.strftime('%H:%M')+  '.'
+  	 @new = ''
+  	 'Hello Team! Your are visitor number ' +time.strftime('%H:%M')+  '.'
  end
 
 

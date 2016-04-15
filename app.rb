dc4159e59e08ba2983dc87fda1725afa6893bf87
vinosri <vinods.scm@gmail.com>
2016-04-14 22:15:27 -0600

    visitor added space

--- app.rb
+++ app.rb
@@ -4,9 +4,9 @@ visitor = 0
 
 get '/' do
   	 time = Time.now
-  	 #Visitor += 1
+  	 Visitor += 1
   	'Hello world! The current time is ' +time.strftime('%H:%M')+ ' GMT' '.' 
-  	'You are Visitor Number ' +visitor.to_s+'.'
+  	'You are Visitor Number ' +visitor.to_s+ '.'
 end
 
 

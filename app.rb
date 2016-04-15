a307ac9c2c0e618efd905c4a42c3106cf1c132a6
vinosri <vinods.scm@gmail.com>
2016-04-15 00:18:06 -0600

    Time and Visitor functionality enhanced

--- app.rb
+++ app.rb
@@ -1,15 +1,8 @@
 require 'sinatra'
 
-$i= 0
+visitor = 0
 get '/' do
   	 time = Time.now
-  	 $i +=1
-  	 #puts  $i
-  	 'Hello world! The current time is ' +time.strftime('%H:%M:%S')+ 'GMT' '\n' '; Hello Team! Your time is '  +time.strftime('%H:%M')+ ','
-  	 #puts ' Great' $i
-  	 # $i '.'
+  	 visitor +=1
+  	 'Hello world! The current time is ' +time.strftime('%H:%M')+ '; You are Visitor Number ' +visitor_to.s+ '.'
  end
-
-
-
-

57783b0dfdde643f6e6ad1ac1a3cbe2842a13675
vinosri <vinods.scm@gmail.com>
2016-04-14 23:10:43 -0600

    Hello team yours in single with new line

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 
 get '/' do
   	 time = Time.now
-  	 'Hello world! The current time is ' +time.strftime('%H:%M')+ ' GMT' '.'\n 
+  	 'Hello world! The current time is ' +time.strftime('%H:%M')+ ' GMT' '.'"\n" 
   	 'Hello Team! Your current time is ' +time.strftime('%H:%M')+  '.'
  end
 

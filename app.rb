52e6259744e1ebf04982aa105955775cc3bd4be3
vinosri <vinods.scm@gmail.com>
2016-04-14 21:37:41 -0600

    visitor function reduced code

--- app.rb
+++ app.rb
@@ -1,14 +1,13 @@
 require 'sinatra'
 get '/' do
-  	time = Time.now
+  	 time = Time.now
   	'Hello world! The current time is ' +time.strftime('%H:%M')+ ' GMT' '.' 
 end
 
 visitor = 0
 
 get '/' do
-  	time = Time.now
-  	visitor += 1
+  	 Visitor += 1
 	'You are Visitor Number ' +visitor.to_s+'.'
 end
 

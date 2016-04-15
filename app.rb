08133c674d2cc2e3913aac8055313c72478ed01e
vinosri <vinods.scm@gmail.com>
2016-04-14 21:58:03 -0600

    visitor function refactored

--- app.rb
+++ app.rb
@@ -6,8 +6,7 @@ get '/' do
   	 time = Time.now
   	 Visitor += 1
   	'Hello world! The current time is ' +time.strftime('%H:%M')+ ' GMT' '.' 
-  	 
-	'You are Visitor Number ' +visitor.to_s+'.'
+  	'You are Visitor Number ' +visitor.to_s+'.'
 end
 
 
